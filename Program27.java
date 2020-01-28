ff3077f65257a437be16b4511ac41c13bd00f466
Rishitha1309 <59167143+Rishitha1309@users.noreply.github.com>
2020-01-28 16:28:10 +0530

    Add files via upload

---
+++ Program27.java
@@ -0,0 +1,24 @@
+import java.util.*;
+import java.io.*;
+class Program27
+{
+public static void main(String args[])
+{
+if(args.length==0)
+{
+System.out.println("Please enter an integer number");
+}
+else
+{
+for(int i=0;i<=Integer.parseInt(args[0]);i++)
+{
+for(int j=0;j<=i;j++)
+{
+System.out.print("*");
+}
+System.out.println();
+}
+}
+}
+
+}
